6b0ab71af2f47ca188417041178551b63e0c3e89
AbhinavvNair <abhinavrajeevunair@gmail.com>
2026-02-14 13:56:35 +0530

    Authentication layer basic applications setup

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
 class Settings:
     GROQ_API_KEY: str = os.getenv("GROQ_API_KEY")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
